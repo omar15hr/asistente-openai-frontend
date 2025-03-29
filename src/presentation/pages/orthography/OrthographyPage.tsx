7cfcaab846d133e92a06bee2caa945ad3ca1d969
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-03-29 16:29:26 -0300

    agregamos el boton y el input para escribir en el chat

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,7 @@
 import { GptMessage } from "../../components/chat-bubbles/GptMessage";
 import { MyMessage } from "../../components/chat-bubbles/MyMessage";
+import { TextMessageBox } from "../../components/chat-input-boxes/TextMessageBox";
+import { TypingLoader } from "../../components/loaders/TypingLoader";
 
 export function OrthographyPage() {
   return (
@@ -8,8 +10,15 @@ export function OrthographyPage() {
         <div className="grid grid-cols-12 gap-y-2">
           <GptMessage text="Hello, how are you today?" />
           <MyMessage text="I'm doing well, thank you for asking!" />
+          <TypingLoader className="fade-in" />
         </div>
       </div>
+
+      <TextMessageBox
+        onSendMessage={(message) => console.log(message)}
+        placeholder="Escribe tu mensaje aquí..."
+        disableCorrections
+      />
     </div>
   );
 }
